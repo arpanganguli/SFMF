a4326b946f8bbbb4db0bf74f501b83fe39db36d9
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-18 23:14:41 +0100

    Edited databasse files.

--- code/single_factor_model.py
+++ code/single_factor_model.py
@@ -2,10 +2,15 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
+# import data
+
 _HOME = os.path.dirname(os.getcwd())
-_URL = 'sqlite:///' + os.path.join(_HOME, 'SFMF/data/PortfolioData.db')
+_URL = 'sqlite:///' + os.path.join(_HOME, 'SFMF/data/database.db')
 engine = create_engine(_URL, echo=True)
 
-query_string = "SELECT * FROM RWA_Input_Data "
-df = pd.read_sql(sql=query_string, con=engine)
-print(df.head())
+query_string_PortfolioData = "SELECT * FROM PortfolioData"
+query_string_CovarianceMatrix = "SELECT * FROM CovarianceMatrix"
+query_string_RSqSens = "SELECT * FROM RSqSens"
+PortfolioData = pd.read_sql(sql=query_string_PortfolioData, con=engine)
+CovarianceMatrix = pd.read_sql(sql=query_string_CovarianceMatrix, con=engine)
+RSqSens = pd.read_sql(sql=query_string_RSqSens, con=engine)
