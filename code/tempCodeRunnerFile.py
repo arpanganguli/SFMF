01819c43ff3d537ac4ac1d4ae26d92ca055c36d3
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-18 22:36:17 +0100

    Update tempCodeRunnerFile.py

--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -1,7 +1,3 @@
 import os
 import pandas as pd
 from sqlalchemy import create_engine
-
-_HOME = os.path.dirname(os.getcwd())
-_URL = 'sqlite:///' + os.path.join(_HOME, 'data/PortfolioData.db')
-engine = create_engine(_URL, echo=True)
