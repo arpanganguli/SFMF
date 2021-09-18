ab4adce5f9b221599b0c93770f0330cb8f936c26
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-18 22:30:43 +0100

    Added import_file.py

---
+++ code/single_factor_model.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+_HOME = os.path.dirname(os.getcwd())
+_URL = 'sqlite:///' + os.path.join(_HOME, 'data/PortfolioData.db')
+engine = create_engine(_URL, echo=True)
+
+query_string = "SELECT * FROM RWA_Input_Data"
+df = pd.read_sql(sql=query_string, con=engine)
+print(df.head())
