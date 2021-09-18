c7c97581ad657637cf0b966c04740f46298c2d2f
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-18 22:39:27 +0100

    Successfully import SQL database into Pandas dataframe.

--- code/single_factor_model.py
+++ code/single_factor_model.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 _HOME = os.path.dirname(os.getcwd())
-_URL = 'sqlite:///' + os.path.join(_HOME, 'data/PortfolioData.db')
+_URL = 'sqlite:///' + os.path.join(_HOME, 'SFMF/data/PortfolioData.db')
 engine = create_engine(_URL, echo=True)
 
-query_string = "SELECT * FROM "
+query_string = "SELECT * FROM RWA_Input_Data "
 df = pd.read_sql(sql=query_string, con=engine)
 print(df.head())
--- code/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-import os
-import pandas as pd
-from sqlalchemy import create_engine
