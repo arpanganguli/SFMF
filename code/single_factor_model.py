fa8c6835e628604d482910639f10e9267dd8919b
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-18 22:36:29 +0100

    Update single_factor_model.py

--- code/single_factor_model.py
+++ code/single_factor_model.py
@@ -6,6 +6,6 @@ _HOME = os.path.dirname(os.getcwd())
 _URL = 'sqlite:///' + os.path.join(_HOME, 'data/PortfolioData.db')
 engine = create_engine(_URL, echo=True)
 
-query_string = "SELECT * FROM RWA_Input_Data"
+query_string = "SELECT * FROM "
 df = pd.read_sql(sql=query_string, con=engine)
 print(df.head())
