41682996401e59817c37612b8efc59e533f1a1d6
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-19 14:57:21 +0100

    First factor sensitivity

--- code/factor_sensitivities.py
+++ code/factor_sensitivities.py
@@ -1,13 +1,54 @@
 """The aim of this module is to calculate factor sensitvity, w_i. This is done using both the Maximum Likelihood Estimation (MLE) technique and calculating it from the Probabilty of Default.
     """
 
+# import relevant packages
 from __init__ import ImportedDataframe
+from sklearn.preprocessing import scale
+import numpy as np
+import pandas as pd
 
-PortfolioData = ImportedDataframe()
-df = PortfolioData.import_sql_data(
+# import relevant data
+
+PortfolioData = ImportedDataframe().import_sql_data(
     'SFMF/data/database.db', 'SELECT * FROM PortfolioData')
 
-MSFTdata = ImportedDataframe()
-MSFT_df = MSFTdata.download_historical_equity_returns(
-    '^GSPC', 'yahoo', '2011-1-1', '2019-12-31')
-print(MSFT_df['Adj Close'].mean())
+Banks = ImportedDataframe().import_sql_data(
+    'SFMF/data/sector_equity_returns.db', 'SELECT * FROM Banks')
+
+Consumer_Goods = ImportedDataframe().import_sql_data(
+    'SFMF/data/sector_equity_returns.db', 'SELECT * FROM ConsumerGoods')
+
+REIT = ImportedDataframe().import_sql_data(
+    'SFMF/data/sector_equity_returns.db', 'SELECT * FROM REIT')
+
+df = PortfolioData.copy()
+
+# Maximum Likelihood Method
+
+# Standardised return of each sector
+
+Banks_standardised_returns = scale(Banks['Change'])
+Consumer_Goods_standardised_returns = scale(Consumer_Goods['Change'])
+REIT_standardised_returns = scale(REIT['Change'])
+
+# Calculating factor sensitivities for each sector
+
+Banks_MLE_w_i = Banks_standardised_returns.mean()
+Consumer_Goods_MLE_w_i = Consumer_Goods_standardised_returns.mean()
+REIT_MLE_w_i = REIT_standardised_returns.mean()
+
+# Adding factor sensitivities to dataframe
+
+choices = [Banks_MLE_w_i, Consumer_Goods_MLE_w_i, REIT_MLE_w_i]
+conditions = [df['Sector'] == 'Banks', df['Sector']
+              == 'Consumer', df['Sector'] == 'Real Estate']
+
+df['Factor_Sensitivity_MLE'] = np.select(conditions, choices)
+
+# Probabilty of Default Method assuming minimum correlation of 0.12 and maximum correlation of 0.24 - (0.12*((1 - np.exp(-50*df['PD'])) / (1 - np.exp(-50)))) + (0.24 * (1 - (1 - np.exp(-50*df['PD'])/(1 - np.exp(-50)))))
+
+exponential = np.exp(-50 * pd.to_numeric(df['PD']))
+common_fraction = (1 - exponential) / (1 - np.exp(-50))
+df['Factor_Sensitivity_PD'] = (
+    0.12 * common_fraction) + (0.24 * (1 - common_fraction))
+print(df.head(10))
--- code/testing.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-
-url = 'https://uk.investing.com/indices/ftse-350-food-producers-tr-historical-data'
-html = requests.get(url).content
-df_list = pd.read_html(html)
-df = df_list[-1]
-print(df)
-df.to_csv('my data.csv')
