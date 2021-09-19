929b9337796ba3e74023d00ddc2f3cf399768ee8
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-19 21:49:05 +0100

    Generated portfolio loss

--- code/single_factor_model.py
+++ code/single_factor_model.py
@@ -1,6 +1,52 @@
+import os
 import numpy as np
 import pandas as pd
 from numpy.random import normal
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('export/factor_sensitivities.csv')
-print(df.head())
+
+HOME = os.getcwd()
+
+df = pd.read_csv('export/factor_sensitivities.csv', index_col=0)
+
+PORTFOLIO_LOSS = list()
+Z = normal(loc=0.0, scale=1.0)
+simulations = 5_000
+
+for i in range(simulations):
+
+    asset_value = list()
+    for row in range(len(df)):
+        epsilon = normal(loc=0.0, scale=1.0)
+        asset_value.append(df['Factor_Sensitivity_MLE'].loc[row]*Z +
+                           np.sqrt(1-pow(df['Factor_Sensitivity_MLE'].loc[row], 2))*epsilon)
+
+    df['Asset_Value'] = asset_value
+
+    default = list()
+    for row in range(len(df)):
+        if df['Asset_Value'].loc[row] < norm.ppf(df['PD'].loc[row]):
+            default.append(1)
+        else:
+            default.append(0)
+
+    df['Default'] = default
+
+    loss = list()
+    for row in range(len(df)):
+        if df['Default'].loc[row] == 1:
+            loss.append(df['LGD'].loc[row]*df['EAD'].loc[row])
+        else:
+            loss.append(0)
+
+    df['Loss'] = loss
+
+    df.to_csv(os.path.join(HOME, 'export', 'defaults.csv'))
+
+    PORTFOLIO_LOSS.append(df['Loss'].sum())
+
+plt.figure(figsize=(25, 10))
+plt.hist(PORTFOLIO_LOSS, bins=100)
+plt.show()
+plt.savefig(os.path.join(HOME, 'export', 'portfolio_loss_distribution.png'))
