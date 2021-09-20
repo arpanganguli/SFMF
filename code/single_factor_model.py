e26f3f0cd70faf12d8e57993b483120f47861aa3
Arpan Ganguli <arpan@alumni.lse.ac.uk>
2021-09-21 00:10:53 +0100

    Added ES 10000

--- code/single_factor_model.py
+++ code/single_factor_model.py
@@ -22,7 +22,7 @@ df = pd.read_csv('export/factor_sensitivities.csv', index_col=0)
 # Monte Carlo simulation
 PORTFOLIO_LOSS = list()
 Z = normal(loc=0.0, scale=1.0)
-simulations = 100
+simulations = 10_000
 
 for i in range(simulations):
 
@@ -89,7 +89,7 @@ plt.axvline(ES_999)
 plt.text(ES_999, -0.4, 'ES 99.9%', rotation=90)
 plt.xlabel('Portfolio Loss')
 plt.ylabel('Frequency')
-plt.title('Portfolio Loss Distribution (100 simulations)')
+plt.title('Portfolio Loss Distribution (10,000 simulations)')
 plt.savefig(os.path.join(HOME, 'export',
-            'portfolio_loss_distribution_100.png'))
+            'portfolio_loss_distribution_10000.png'))
 plt.show()
